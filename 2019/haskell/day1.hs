e5ca9a41c076fcae7d74c7e0d23032bd776b2a6f
Huibert - Jan Joosse <hjjoosse@Huiberts-MBP.fritz.box>
2021-01-20 20:34:50 +0100

    day1 done in haskell

---
+++ 2019/haskell/day1.hs
@@ -0,0 +1,24 @@
+import System.IO
+
+fuelAdder :: Integer -> Integer
+fuelAdder x = div x 3 - 2
+
+totalFuel :: Integer -> Integer 
+totalFuel x 
+    | fuelToadd <= 0    = 0
+    | otherwise         = fuelToadd + totalFuel fuelToadd
+    where fuelToadd = fuelAdder x
+
+solvePart1 :: [Integer] -> Integer
+solvePart1 = sum . map fuelAdder
+
+solvePart2 :: [Integer] -> Integer 
+solvePart2 = sum . map totalFuel
+
+day1solver :: String -> IO ()
+day1solver fileName = do
+    inputLines <- readFile fileName
+    let inputList = lines inputLines
+    let masses = map read inputList :: [Integer]
+    print (solvePart1 masses)
+    print (solvePart2 masses)
