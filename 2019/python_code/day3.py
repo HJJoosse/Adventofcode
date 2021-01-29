c5fc2265b744679ff3ac088f495fceefc56076e5
Huibert - Jan Joosse <hjjoosse@Huiberts-MacBook-Pro.fritz.box>
2021-01-29 11:36:09 +0100

    optimized python code

--- 2019/python_code/day3.py
+++ 2019/python_code/day3.py
@@ -1,4 +1,5 @@
 import operator
+from collections import ChainMap, Counter
 
 def x_or_y(direction:str,distance:int,last_pos:list):
     if direction in ["L","R"]:
@@ -17,39 +18,43 @@ def x_or_y(direction:str,distance:int,last_pos:list):
     
     return [(x,y) for x,y in zip(x_coords,y_coords)]
 
-def create_route(wire: list, route:list, steps: int):
+def create_route(wire: list, route:dict, counter:Counter, steps: int):
     direction = wire[0][0]
     distance = wire[0][1]
     wire = wire[1:]
-    last_pos = route[-1]
-    route += x_or_y(direction,distance,last_pos)
+    last_pos = route[steps][-1]
+    
     steps += 1
+    route.update({steps:x_or_y(direction,distance,last_pos)})
+    counter.update(route[steps])
+    
     return route, wire,steps 
 
 def get_euclidean(intup : tuple):
     return sum([abs(x) for x in intup])
 
-def track_route(wire_one: list, wire_two: list,route_one: list, route_two: list,steps_a:int = 0,steps_b:int = 0):
+def track_route(wire_one: list, wire_two: list,route_one: dict, route_two: list,steps_a:int = 0,steps_b:int = 0):
+    ct = Counter()
     while (len(wire_one) > 0):
-        route_one, wire_one, steps_a = create_route(wire_one, route_one,steps_a)
+        route_one, wire_one, steps_a = create_route(wire_one, route_one, ct,steps_a)
     overlap = []
 
     for x in wire_two:
         route_two = x_or_y(x[0],x[1],route_two[-1])
         for coord in route_two:
             steps_b +=1
-            if coord in route_one:
-                step_on_a = route_one.index(coord)
+            if coord in ct.keys():
+                step_on_a = list(ct.keys()).index(coord)+1  
                 overlap.append([get_euclidean(coord),step_on_a+steps_b])
     return overlap
 
 if __name__ == "__main__":
     with open("../inputs/input_d3","r") as f:
        wire_one, wire_two = f.read().split("\n")[0:2]
-    #wire_one = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
-    #wire_two = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+    #wire_one = "R75,D30,R83,U83,L12,D49,R71,U7,L72"
+    #wire_two = "U62,R66,U55,R34,D71,R55,D58,R83"
     wire_one = [(x[0],int(x[1:])) for x in wire_one.split(",")]
     wire_two = [(x[0],int(x[1:])) for x in wire_two.split(",")]
-    intersects = (track_route(wire_one,wire_two,[(0,0)],[(0,0)]))
+    intersects = (track_route(wire_one,wire_two,{0:[(0,0)]},[(0,0)]))
     print("Answer part 1:",min([x[0] for x in intersects]))
     print("Answer part 2:",min([x[1] for x in intersects]))
