802a2a057462c8bf23fdd96cfe8dcff90bcf1474
hjjoosse <huibertjanjoosse@gmail.com>
2021-12-05 18:04:03 +0100

    start day4 in go

---
+++ 2021/golang/day4/day4.go
@@ -0,0 +1,15 @@
+package day4
+
+import (
+	"aoc21/utils"
+	"fmt"
+)
+
+func SolveDay4() {
+	fmt.Println(part1("/Users/hjjoosse/Documents/UMCU/Adventofcode/2021/data/input_d4.txt"))
+}
+
+func part1(path string) {
+	arr := utils.ReadStringArray(path)
+	return arr
+}
